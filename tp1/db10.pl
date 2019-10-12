9243ea6238c9c04137e4df2e689b960aae5fb6de
jose <up201706421@fe.up.pt>
2019-10-12 16:00:11 +0100

    Organized the T1 folder

--- tp1/db10.pl
+++ tp1/db10.pl
@@ -4,10 +4,10 @@ comprou(joao, uno).
 ano(honda, 2010). 
 ano(uno, 2011).
 
-valor(honda, 20000).
+valor(honda, 10000).
 valor(uno, 7000). 
 
 podeVender(Pessoa, Carro, Ano) :- 
     comprou(Pessoa, Carro), 
-    valor(Carro, X), X < 10000, 
+    valor(Carro, X), X =@= 10000, 
     ano(Carro, Y), Y > 2009, Y < 2019. 
