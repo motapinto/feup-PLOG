f211b9985ca38637df7565d4b2c75b780150c3f1
José Guerra <guerra70@live.com.pt>
2019-10-26 14:42:15 +0100

    rules

--- Projects/Project1/src/rules.pl
+++ Projects/Project1/src/rules.pl
@@ -6,23 +6,22 @@ checkIfNotNull(Row, Column, ErrorType):-
 
 
 checkRules(Row, Column, ErrorType):-
-    ErrorType = 0,
     checkIfNotNull(Row, Column, ErrorType).
 
 checkColumn(1, [H|T], ErrorType):-
     (H == nullCell ->
     ErrorType = 1,
-    fail; true).
+    fail; ErrorType = 0, true).
 
 checkColumn(Column, [H|T] , ErrorType):-
     Column > 1,
     ColumnI is Column - 1, 
-    checkColumn(ColumnI, T).
+    checkColumn(ColumnI, T, ErrorType).
 
 checkRow(1, Column, [H|T], ErrorType):-
-    checkColumn(Column, H).
+    checkColumn(Column, H, ErrorType).
     
 checkRow(Row, Column, [H|T] , ErrorType):-
     Row > 1,
     RowNext is Row - 1, 
-    checkRow(RowNext, Column, T).
+    checkRow(RowNext, Column, T, ErrorType).
