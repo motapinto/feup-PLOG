72e39afd0b727d33983ea22d7464ec8ac43788cc
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-11-09 17:22:15 +0000

    machine still not working

--- Projects/Project1/src/machine.pl
+++ Projects/Project1/src/machine.pl
@@ -71,43 +71,86 @@ nextPos(Row, Column, RowN, ColumnN) :-
             if_then_else(RowN > 11, fail, true)
         )
     ).
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   FIRST POSSIBLE MOVE
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Returns first possible move from possibleMovesPX from player
+firstPossibleMove(Row, Column, Player) :-
+    if_then_else(
+        Player == 1,
+        (
+            %   Player 1
+            possibleMovesP1(Init),
+            checkRow(Init, Row, Column, 0)
+        ),
+        (
+            %   Player 2
+            possibleMovesP2(Init),
+            checkRow(Init, Row, Column, 0) 
+        ) 
+    ).
 
+%   Checks first column in received row that as a possible move
+checkCol([H|T], Column, Counter):-
+    CounterN is Counter + 1,
+
+    if_then_else(
+        H == y, 
+        (true, Column = CounterN),
+        checkCol(T, Column, CounterN)
+    ).
+
+%   Checks first row in which there is a column that as a possible move
+checkRow([H|T], Row, Column, Counter):-
+    CounterN is Counter + 1,
+
+    if_then_else(
+        checkCol(H, Column, 0),
+        (true, Row = CounterN),
+        checkRow(T, Column, Row, CounterN)
+    ).
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   ADDS A POSSIBLE MOVE
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Updates possibleMovesPX depending on the player
 addPossibleMove(Row, Column, Player) :-
     if_then_else(
         Player == 1,
         (
             %   Player 1
             retract(possibleMovesP1(Before)),
-            addMove(Before, After, Row, Column, Player),
+            addMove(Before, After, Row, Column),
             assert(possibleMovesP1(After))
         ),
         (
             %   Player 2
             retract(possibleMovesP1(Before)),
-            addMove(Before, After, Row, Column, Player),
+            addMove(Before, After, Row, Column),
             assert(possibleMovesP1(After))
         ) 
     ).
-
-%   Returns first possible move from possibleMovesPX from player
-%firstPossibleMove(Row, Column, Player) :-
-
 %   Adds possible move to possibleMovesPX from player
-addMove(Before, After, Row, Column, Player) :-
-    updateRowPossibleMoves(Row, Column, Before, After, Player).
+addMove(Before, After, Row, Column) :-
+    updateRowPossibleMoves(Row, Column, Before, After).
 
 %   Iterates through the columns of the possibleMovesPX, adding the possible move to possibleMovesPX
-updateColumnPossibleMoves(1, [H|T], [Hout|T], Color):-
+updateColumnPossibleMoves(1, [H|T], [Hout|T]):-
     Hout = y.
-updateColumnPossibleMoves(Column, [H|T], [H|Tout], Color):-
+updateColumnPossibleMoves(Column, [H|T], [H|Tout]):-
     Column > 1,
     ColumnI is Column - 1, 
-    updateColumnPossibleMoves(ColumnI, T, Tout, Color).
+    updateColumnPossibleMoves(ColumnI, T, Tout).
 
 %   Iterate through the rows of the possibleMovesPX 
-updateRowPossibleMoves(1, Column, [H|T], [Hout|T], Color):-
-    updateColumnPossibleMoves(Column, H, Hout, Color).    
-updateRowPossibleMoves(Row, Column, [H|T], [H|Tout], Color):-
+updateRowPossibleMoves(1, Column, [H|T], [Hout|T]):-
+    updateColumnPossibleMoves(Column, H, Hout).    
+updateRowPossibleMoves(Row, Column, [H|T], [H|Tout]):-
     Row > 1,
     RowNext is Row - 1, 
-    updateRowPossibleMoves(RowNext, Column, T, Tout, Color).
+    updateRowPossibleMoves(RowNext, Column, T, Tout).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
