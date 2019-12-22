ca54306d81f545bb79b1996b8d2abe216b22f6ca
LockDownPT <48472671+LockDownPT@users.noreply.github.com>
2019-12-22 12:32:33 +0000

    Update prob1.pl

--- Practical/tp8_PLR/prob1.pl
+++ Practical/tp8_PLR/prob1.pl
@@ -28,16 +28,63 @@ magic(Vars) :-
 % N by N version
 magic(N, Vars) :-
     Limit is N*N,
-    length(Vars, N),
+    length(Vars, Limit),
     domain(Vars, 1, Limit),
-
-    restrictions(Vars),
     all_distinct(Vars),
+    
+    sumLines(Vars, N, 0, Soma), 
+    sumColumns(Vars, N, 0, Soma),
+    sumDiagonal1(Vars, N, 0, Soma),
+    sumDiagonal2(Vars, N, 0, Soma), 
+    
     labeling([], Vars).
 
-restrictions([]).
-restrictions([H|T]) :-
-    checkLine(H),
-    restrictions(T).
+sumLines(_, LineSize, LineSize, _).
+sumLines(Vars, LineSize, Line, TotalSum):-
+    sumLine(Vars, LineSize, 0, Line, TotalSum),
+    LineAux is Line + 1,
+    sumLines(Vars, LineSize, LineAux, TotalSum).
 
-checkLine([H|T])
+
+sumLine(_, LineSize,  LineSize, _ , 0).
+sumLine(Vars, LineSize, Counter, Line, TotalSum):-
+    CounterAux is Counter + 1,
+    sumLine(Vars, LineSize, CounterAux, Line, TotalSumAux),
+    Index is LineSize * Line + Counter,
+    nth0(Index, Vars, Elem),
+    TotalSum #= TotalSumAux + Elem.
+
+
+sumColumns(_, ColumnSize, ColumnSize, _).
+sumColumns(Vars, ColumnSize, Column, TotalSum):-
+    sumColumn(Vars, ColumnSize, 0, Column, TotalSum),
+    ColumnAux is Column + 1,
+    sumColumns(Vars, ColumnSize, ColumnAux, TotalSum).
+
+
+sumColumn(_, ColumnSize,  ColumnSize, _ , 0).
+sumColumn(Vars, ColumnSize, Counter, Column, TotalSum):-
+    CounterAux is Counter + 1,
+    sumColumn(Vars, ColumnSize, CounterAux, Column, TotalSumAux),
+    Index is ColumnSize * Counter + Column,
+    nth0(Index, Vars, Elem),
+    TotalSum #= TotalSumAux + Elem.
+
+sumDiagonal1(_, DiagonalSize, DiagonalSize, 0).
+sumDiagonal1(Vars, DiagonalSize, Counter, TotalSum):-
+    CounterAux is Counter + 1,
+    sumDiagonal1(Vars, DiagonalSize, CounterAux, TotalSumAux),
+    Index is DiagonalSize * Counter + Counter,
+    nth0(Index, Vars, Elem),
+    TotalSum #= TotalSumAux + Elem.
+
+sumDiagonal2(_, DiagonalSize,  DiagonalSize, 0).
+sumDiagonal2(Vars, DiagonalSize, Counter, TotalSum):-
+    CounterAux is Counter + 1,
+    sumDiagonal2(Vars, DiagonalSize, CounterAux, TotalSumAux),
+    Index is (DiagonalSize - 1) * (Counter + 1),
+    nth0(Index, Vars, Elem),
+    TotalSum #= TotalSumAux + Elem.
+
+
+
