90586454e839ebf96c3a5d2078cd755f54506aad
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-12-23 21:46:45 +0000

    commit

--- Projects/Project2/src/lib.pl
+++ Projects/Project2/src/lib.pl
@@ -7,7 +7,7 @@ print_time :-
 	TS is ((T // 10) * 10) / 1000,
     nl, write('Solution Time: '), write(TS), write('s'), nl, nl.
 
-clear_console:- 
+clear:- 
     clear_console(100), !.
 
 clear_console(0).
@@ -15,3 +15,40 @@ clear_console(N):-
 	nl,
 	N1 is N-1,
 	clear_console(N1).
+
+hasNoLeftDot(Index, Vars, N) :-
+	% Left index cannot be a dot
+	IndexN is Index - 1,
+	piece(Dot, '*'),
+	nth0(IndexN, Vars, Elem),
+	Elem \= Dot,
+	% Checks if has reached end of column
+	Col is Index mod N,
+	End is N - 1;
+	Index == End -> true ; hasLeftDot(IndexN, Vars).
+
+hasNoRightDot(Index, Vars) :-
+	limitCol(Index, Vars),
+	nth0(Index, Vars, Elem),
+	IndexN is Index + 1,
+	hasLeftDot(IndexN, Vars).
+
+checkLimits(Index, N):-
+	limitLine(Index, N),
+	limitCol(Index, N).
+
+limitCol(Index, N) :-
+	% checking if index is on the first column
+	FirstColumnn is Index mod N,
+	FirstColumnn \= 0,
+	% checking if index is on the last column
+	LineNumber is Index // N,
+	LastColumn is N * LineNumber + N - 1,
+	Index \= LastColumn.
+
+limitLine(Index, N) :-
+	% checking if index on the first line
+	Index >= N,
+	% checking if index is on the last line
+	Limit is N*N,
+	Index <  Limit - N.
---
+++ Theory/plr.pl
@@ -0,0 +1,61 @@
+:-use_module(library(clpfd)).
+
+map_coloring(L) :-
+    L = [WA, NT, SA, Q, NSW, V, T],
+    domain(L, 0, 2), % equivalente a fazer L in 0..2
+    WA #\= NT, WA#\=SA,
+    NT #\=NT, NT#\=Q,
+    write('.'), %ver quantas vezes faz p backtracking -> so imprime uma vez o '.'
+    labeling([], L).
+
+% % se fizermos primeiro o labelling o programa fica menos eficiente
+% %e sao impressos muitos mais pontos pq havera muito backtracking
+% % mal haja uma restricao que falhe faz backtrcking
+
+% %fazendo este metodo se pedir outra solucao o numero de pontos impressos
+% % vai variar devido ao backtracking
+
+% %terceira solucao -> prolog de teste 1
+
+% % map_coloring(WA, NT, SA, Q, NSW, V, T):-
+% %     MEMBER(...)
+
+% %esta solucao tambem faz muito backtracking e e parecido com a segunda solucao(extamanete igual)
+% %isto é fazer primeiro o labelling e dps as restricoes
+
+% %POR ISSO E IMPORTNATE FAZER PRIMEIRO RESTRICOES E DPS LABELLING -> nao ha backtracking
+
+
+
+
+% % se fizermos n ele faz backtracking ate ao labelling
+
+
+% %podemosfazer A in(2,8) \/ (15..20)
+% %A in {4,8,14,24,28}
+
+% in_set()% conjunto de valores posssiveis
+% Numbers = [5, 6, 7, 19, 25, 56]
+% list_to_fdset(Numbers, FDS_Numbers),
+% ?
+
+% domain(List, Min, Max)
+
+% fd_batch?
+
+% reified: restricao nao colocada e variavel a 0
+% restricao colocada ou variavel a 1
+% constraint #<=> B
+% B pode ter valor 0 ou 1
+
+% restricoes globais a maior parte delas nao pode ser materializadas
+% as restricoes simples podem ser materializadas
+
+
+% exactly(X,L,N). % numero de vezes que X aparce em L
+
+exactly(_, [], 0).
+exactly(X, [Y|L], N) :-
+    X #= Y #<=>B,
+    N #= M+B,
+    exactly(X,L,M).
