10ed79bf45d5df4aa332e692855102fe97fde0b7
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-12-23 18:56:23 +0000

    Update lib.pl

--- Projects/Project2/src/lib.pl
+++ Projects/Project2/src/lib.pl
@@ -6,3 +6,12 @@ print_time :-
 	statistics(walltime,[_,T]),
 	TS is ((T // 10) * 10) / 1000,
     nl, write('Solution Time: '), write(TS), write('s'), nl, nl.
+
+clear_console:- 
+    clear_console(100), !.
+
+clear_console(0).
+clear_console(N):-
+	nl,
+	N1 is N-1,
+	clear_console(N1).
