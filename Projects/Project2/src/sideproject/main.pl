b9228e973fc265ae3764916e28e0a9c277d06a88
José Guerra <guerra70@live.com.pt>
2019-12-26 15:28:34 +0000

    done sublit our function

--- Projects/Project2/src/sideproject/main.pl
+++ Projects/Project2/src/sideproject/main.pl
@@ -23,11 +23,11 @@ main(N, Board):-
     %generate_board(N, Board),
     
     Board = [
-        [A0, 4, A2, A3, A4], 
+        [A0, A1, A2, A3, A4], 
         [A5, A6, A7, A8, A9], 
-        [4, A11, 4, A13, A14], 
+        [A10, A11, A12, A13, A14], 
         [A15, A16, A17, A18, A19], 
-        [A20, 4, A22, A23, A24]
+        [A20, A21, A22, A23, A24]
     ],
     % a passar tudo só para uma lista
     append(Board, OneListBoard),
@@ -35,17 +35,17 @@ main(N, Board):-
     
     printMatrixes(Board, TransposeBoard, OneListBoard),
 
-
-    % restringe, nas bordas do board só pode estar a letra O ou um ponto
-    Limit is N * N,
-    restrictBorders(OneListBoard , 0, Limit, N),
-
-    %tentar fazer as restirçoes mid, O, N
-   
     % impor as restrições para a linhas e para as colunas
     % colunas e linhas só podem ter no máximo uma letra e dois pontos
     iterateListOfListsForDomain(Board),
     iterateListOfListsForDomain(TransposeBoard), 
+
+    % restringe, nas bordas do board só pode estar a letra O ou um ponto
+    Limit is N * N,
+    restrictionsPos(OneListBoard , Board, TransposeBoard, 0, Limit, N),
+
+    %tentar fazer as restirçoes mid, O, N
+   
 
     % fazer o labeling de só uma lista
     labeling([], OneListBoard),
@@ -55,14 +55,15 @@ main(N, Board):-
     % dar print do board, Atenção da print de uma lista de listas
     once(printBoard(Board, N)).
 
-restrictionsPos(_, Limit, Limit, N).
-restrictionsPos(OneListBoard, Index, Limit, N):-
+restrictionsPos(_, _, _, Limit, Limit, N).
+restrictionsPos(OneListBoard, Board, TransposeBoard, Index, Limit, N):-
     nth0(Index, OneListBoard, Elem),
     %restrictMidpoint(OneListBoard, Index, N, Elem),
-    restrictOpoint(OneListBoard, Index, N, Elem),
+    %restrictOpoint(Board, TransposeBoard, Index, N, Elem),
+    restrictOpoint(OneListBoard, Index, N),
     %restrictNpoint()
     IndexAux is Index + 1,
-    restrictBorders(OneListBoard, IndexAux, Limit, N).
+    restrictionsPos(OneListBoard,  Board, TransposeBoard, IndexAux, Limit, N).
 
 iterateListOfListsForDomain([]).
 iterateListOfListsForDomain([H | T]):-
@@ -87,16 +88,12 @@ iterateListOfListsForDomain([H | T]):-
             arc(n3, 2, n6),
             arc(n3, 3, n6),
 
-
             arc(n4, 0, n4),
             arc(n4, 4, n5),
 
-
             arc(n5, 0, n5),
             arc(n5, 4, n6),
-        
-
-
+    
             arc(n6, 0, n6)
         ]
     ),
@@ -111,27 +108,73 @@ checkingLetter(Vars, Index, N):-
     isMidpoint(Vars, Index, N).
     % isOpoint(Vars, Index, N).
 
+countingNumberDots(List, N):-
+    automaton(List, _, List,
+    [source(s), sink(s)],
+    [
+        arc(s,0,s),
+        arc(s,1,s),
+        arc(s,2,s),
+        arc(s,3,s),  
+        arc(s,4,s,[C+1])
+    ],
+    [C],[0],[N]).
 
-restrictOpoint(Vars, Index, N):-
 
-    RightPos is Index + 1,
-    LeftPos is Index - 1, 
-    UpPos is Index - N,
-    DownPos is Index + N,
-    piece(LetterO, 'O'),
+
+% Para efeitos de teste
+% restrictOpoint(Board,  Index, N):-
+
+%     transpose(Board, TransposeBoard),
+
+
+restrictOpoint(Board, TransposeBoard, Index, N, Elem):-
+    
     Line is Index // N,
-    LengthLeft is Index - Line * N, 
-    LengthTop = Line,
-    LengthBottom is N - LengthTop - 1,
-    LengthRight is N - LengthLeft - 1,
-        
-    hasTopDot(UpPos, Vars, N, 0, LengthTop, DotSumTop),
-    hasBottomDot(DownPos, Vars, N, 0,  LengthBottom, DotSumBottom),
-    hasRightDot(RightPos, Vars, N,  0, LengthRight, DotSumRight),
-    hasLeftDot(LeftPos, Vars, N,  0, LengthLeft, DotSumLeft),
-    nth0(Index, Vars, Elem),
+    Column is Index - Line * N,
+    nth0(Line, Board, LineList),
+    nth0(Column, TransposeBoard, ColumnList),
+    
+    LengthLeft is Index - Line * N + 1, 
+    LengthTop is Line + 1,
+    LengthBottom is N - LengthTop + 1,
+    LengthRight is N - LengthLeft + 1,
+    
+    sublistOur(LineList, LineListLeft, 0, 0,),
+    sublistOur(LineList, LineListRight, ColumnAux, 0, ),
+    sublistOur(ColumnList, ColumnListTop, 0, 0, Line),
+    sublistOur(ColumnList, ColumnListBottom, LineAux, N),
 
-    ((DotSumLeft #= 2 #/\ DotSumTop #= 2) #\/ (DotSumLeft #= 2 #/\ DotSumBottom #= 2) #\/ (DotSumRight #= 2 #/\ DotSumTop #= 2) #\/ (DotSumRight #= 2 #/\ DotSumBottom #= 2)) #<=> Elem #= LetterO.
+    
+    countingNumberDots(LineListLeft, LineLeftCounterDots),
+    countingNumberDots(LineListRight, LineRightCounterDots),
+    countingNumberDots(ColumnListTop, ColumnTopCounterDots),
+    countingNumberDots(ColumnListBottom, ColumnBottomCounterDots).
+
+    print(LineLeftCounterDots),
+    print(LineRightCounterDots),
+    print(ColumnTopCounterDots),
+    print(ColumnBottomCounterDots).
+    
+    piece(LetterO, 'O'),
+    ((LineLeftCounterDots #= 0 #\/ LineRightCounterDots #= 0) #/\ (ColumnTopCounterDots #= 0 #\/ ColumnBottomCounterDots #= 0)) #<=> Elem #= LetterO.
+
+
+
+sublistOur(List, [], Start, Length, Length).
+sublistOur(List,[H | T], Start, CounterLenght, Length):-
+    Index is Start +  CounterLenght,
+    CounterLenghtAux is CounterLenght + 1,
+    sublistOur(List, T, Start, CounterLenghtAux, Length),
+    nth0(Index, List, Elem),
+    H = Elem.
+
+
+
+
+
+
+
 
 
 % Vars  -> Board
@@ -154,41 +197,7 @@ isMidpoint(Vars, Index, N) :-
 
             minimum(MinHorizontal, LengthHorizontal),
             minimum(MinVertical, LengthVertical),
-            checkMidPoint(Index, N, Vars, 1, MinHorizontal, MinVertical)
-            automaton(H, 
-        [source(n1), sink(n6)], 
-        [
-            arc(n1, 0, n1),
-            arc(n1, 4, n2),
-            arc(n1, 1, n4),
-            arc(n1, 2, n4),
-            arc(n1, 3, n4),
-
-            arc(n2, 0, n2),
-            arc(n2, 4, n3),
-            arc(n2, 1, n5),
-            arc(n2, 2, n5),
-            arc(n2, 3, n5),
-
-            arc(n3, 0, n3),
-            arc(n3, 1, n6),
-            arc(n3, 2, n6),
-            arc(n3, 3, n6),
-
-
-            arc(n4, 0, n4),
-            arc(n4, 4, n5),
-
-
-            arc(n5, 0, n5),
-            arc(n5, 4, n6),
-        
-
-
-            arc(n6, 0, n6)
-        ]
-    ),
-            
+            checkMidPoint(Index, N, Vars, 1, MinHorizontal, MinVertical)         
         );
         ( 
             nth0(Index, Vars, Elem),
