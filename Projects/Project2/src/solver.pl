0604bc3365a24ece5c311231b366e49d08a1502a
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2019-12-29 23:49:08 +0000

    final solver commit

--- Projects/Project2/src/solver.pl
+++ Projects/Project2/src/solver.pl
@@ -5,13 +5,18 @@
 :-[lib].
 :-[puzzles].
 
-solver(Matrix, N) :-
-    % Resets time for statistics
-    reset_timer,
+main(Matrix, N) :-
     % Generates a board bazed on predefined boards
     generate_board(N, Selected, Matrix),
     once(printBoard(Selected, N)),
-    sleep(2),
+    % Solves the random puzzle
+    solver(Matrix, N),
+    % prints the game Board
+    once(printBoard(Matrix, N)).
+
+solver(Matrix, N) :-
+    % Resets time for statistics
+    reset_timer,
     % Stores the matrix as 1 dimension
     append(Matrix, OneListMatrix),
     % Specifies the domain of the matrix
@@ -23,84 +28,12 @@ solver(Matrix, N) :-
     solveMatrix(TMatrix), 
     % labeling of the one list matrix
     !, labeling([], OneListMatrix),
-    % prints the game Board
-    once(printBoard(Matrix, N)),
     % prints elapsed time
     print_time.
 
 % Solves received matrix (solves only the lines)
 solveMatrix([]).
 solveMatrix([H|T]) :-
-    length(H, LineLength),
-    %solveLine(H, 0, LineLength),
-    dotDistance(H),
-    solveMatrix(T).
-
-% Restricts each line (2 dots and 1 letter per line + restricts line)
-solveLine(_, LineLength, LineLength).
-solveLine(Line, Index, LineLength) :-
-    nth0(Index, Line, IndexElem),
-    piece(M, 'M'),
-    piece(N, 'N'),
-    piece(O, 'O'),
-    piece(Dot, '*'),
-    piece(Null, ' '),
-
-    %allPointsAutomata(Line),
-
-    NextIndex is Index + 1,
-    solveLine(Line, NextIndex, LineLength).
-
-% Automaton for M,N and O points
-allPointsAutomata(Line) :-
-    automaton(Line, _, Line, [source(s), sink(s3), sink(o2)],
-        [
-            arc(s, 0, s),
-            arc(s, 1, o3),
-            arc(s, 4, s1),
-            
-            arc(s1, 0, s1, [C+1]),
-            arc(s1, 2, s2),
-            arc(s1, 3, s4),
-            arc(s1, 4, o1),
-
-            arc(s2, 0, s2, [C-1]),
-            arc(s2, 4, s3),
-
-            arc(o1, 0, o1),
-            arc(o1, 1, o2),
-
-            arc(o2, 0, o2),
-
-            arc(o3, 0, o3),
-            arc(o3, 4, o4),
-
-            arc(o4, 0, o4),
-            arc(o4, 4, o2),
-
-            arc(s3, 0, s3),
-
-            arc(s4, 0, s4, [C-1]),
-            arc(s4, 4, s3)
-        ],
-        [C], [0], [N]
-    ).
-
-teste(List):-
-    reset_timer,
-    generate_board(5, Selected, List),
-    once(printBoard(Selected, 5)),
-    append(List, OneList),
-    domain(OneList, 0, 4),
-    dotDistance(List),
-    transpose(List, TList),
-    dotDistance(TList), !,
-    labeling([], OneList),
-    once(printBoard(List, 5)),
-    print_time.
-
-dotDistance([]).
-dotDistance([H | T]):-
     automaton(H, _, H, [source(s), sink(s3), sink(o2)],
         [
             arc(s, 0, s),
@@ -132,28 +65,13 @@ dotDistance([H | T]):-
             arc(s4, 4, s3)
         ],
         [C], [0], [DeltaDist]
-    ),
-    length(H, Length),
+    ),    
+    solveLine(H),
+    solveMatrix(T).
 
-    iterateList(H, DeltaDist, 0, Length),
-    dotDistance(T).
-
-iterateList(_, _, Length, Length).
-iterateList(List, DeltaDist, Counter, Lenght):-
-    nth0(Counter, List, Elem),
-
-    DeltaDist #= 0 #<= Elem #= 2,
-    DeltaDist #\=0 #<= Elem #= 3,
-
-    NextCounter is Counter + 1,
-    iterateList(List, DeltaDist, NextCounter, Lenght).
-
-
-
-
-    
-
-    
-    
-
-    
+% Restricts each line (2 dots and 1 letter per line + restricts line)
+solveLine([], _).
+solveLine([H | T], DeltaDist):-  
+    DeltaDist #= 0 #<= H #= 2,
+    DeltaDist #\=0 #<= H #= 3,
+    solveLine(T).
